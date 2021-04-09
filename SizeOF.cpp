136174583b3638c82855c666da4ecaf9de77e6b8
Roshan Roy <79837320+695roshan@users.noreply.github.com>
2021-04-09 21:46:48 +0530

    To Show the size of different data types

---
+++ SizeOF.cpp
@@ -0,0 +1,11 @@
+// C++ Program to Find Size of int, float, double and char
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout << "Size of char: "<<sizeof(char)<<" byte\n";
+    cout << "Size of int: "<<sizeof(int)<<" bytes\n";
+    cout << "Size of float: "<<sizeof(float)<<" bytes\n";
+    cout << "Size of double: "<<sizeof(double)<<" bytes\n";
+    return 0;
+}
